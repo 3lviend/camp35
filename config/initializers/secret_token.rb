f3975d3488891173b904586b9c9b58f044f722ad
Kamil Ciemniewski <kamil@endpoint.com>
2012-09-20 10:54:27 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimesheetApp::Application.config.secret_token = '026a0655ef5d8dd77d60bf00e14d006285ffa351634d000f83d66888538003d1341fe28de5eafbb1c7256affd2b08c99b8c8e53fd0171be17db0e5b7492e7d91'
