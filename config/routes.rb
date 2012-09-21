0bb5295e2c465cc4d28bce784407dea433f52d25
Kamil Ciemniewski <kamil@endpoint.com>
2012-09-21 16:10:51 -0400

    Added pagination to week view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ TimesheetApp::Application.routes.draw do
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 
   root :to => "work_days#index"
-  match "/work_days/:weeks_from_now" => "work_days#index"
+  match "/work_days/:weeks_from_now" => "work_days#index", as: :week_work_days
   devise_for :users
 end
