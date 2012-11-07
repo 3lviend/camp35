a5cdea236ce181853984191667d3fd86cf8b047a
Kamil Ciemniewski <kamil@endpoint.com>
2012-11-07 18:42:13 -0500

    Speeded up a bit charts tree building

--- app/controllers/work_chart_duration_kinds_controller.rb
+++ app/controllers/work_chart_duration_kinds_controller.rb
@@ -1,5 +1,6 @@
 class WorkChartDurationKindsController < ApplicationController
   respond_to :json
+  caches_page :index
   def index
     @kinds = WorkChart.find(params[:work_chart_id]).duration_kinds # WorkChartKind.where(work_chart_id: params[:work_chart_id]).map(&:duration_kind)
     respond_with @kinds
--- app/controllers/work_charts_controller.rb
+++ app/controllers/work_charts_controller.rb
@@ -3,12 +3,24 @@ class WorkChartsController < ApplicationController
   respond_to :json
 
   def index
-    base = params[:include_inactive] ? WorkChart.where({}) : WorkChart.where(status: "active")
-    respond_with base.where(parent_id: params[:parent_id]).order(:display_label)
+    key = "work_charts_#{params[:include_inactive]}_#{params[:parent_id]}"
+    @charts = Rails.cache.read key
+    unless @charts
+      base = params[:include_inactive] ? WorkChart.where({}) : WorkChart.where(status: "active")
+      @charts = base.where(parent_id: params[:parent_id]).order(:display_label)
+      Rails.cache.write key, @charts, :expires_in => 2.days
+    end
+    respond_with @charts
   end
 
   def show
-    respond_with WorkChart.find params[:id]
+    key = "work_chart_#{params[:id]}"
+    @chart = Rails.cache.read key
+    unless @chart
+      @chart = WorkChart.find params[:id]
+      Rails.cache.write key, @chart, :expires_in => 2.days
+    end
+    respond_with @chart
   end
 
   def search
@@ -16,11 +28,23 @@ class WorkChartsController < ApplicationController
   end
 
   def frequent
-    respond_with WorkChart.frequent_for(current_user, 20)
+    key = "user_frequent_#{current_user.id}"
+    @frequents = Rails.cache.read key
+    unless @frequents
+      @frequents = WorkChart.frequent_for(current_user, 20)
+      Rails.cache.write key, @frequents, :expires_in => 2.days
+    end
+    respond_with @frequents
   end
 
   def recent
-    respond_with WorkChart.recent_for(current_user, 20)
+    key = "user_recent_#{current_user.id}"
+    @recents = Rails.cache.read key
+    unless @recents
+      @recents = WorkChart.recent_for(current_user, 20)
+      Rails.cache.write key, @recents, :expires_in => 2.days
+    end
+    respond_with @recents
   end
 
   def duration_kinds
