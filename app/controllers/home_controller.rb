fcfebae7deb5a1566c5b9ed6485a1f142d060f63
Kamil Ciemniewski <kamil@endpoint.com>
2012-12-18 07:26:09 -0500

    Married back-end and front-end apps in one camp

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   #before_filter :ensure_logged_in
 
   def index
-    render :file => File.join(Rails.root, 'public', 'brunch', 'TimesheetApp', 'public', 'index.html'), :layout => false
+    render :file => File.join(Rails.root, 'public', 'brunch', 'public', 'index.html'), :layout => false
   end
 end
